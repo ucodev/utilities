d83b3018f682927dd1226546c8358ccb34b6dd51
Pedro A. Hortas <pah@ucodev.org>
2014-09-19 13:08:58 +0100

    Updated ODBC configuration and connection strings

--- config/unixodbc/example/test_mysql.py
+++ config/unixodbc/example/test_mysql.py
@@ -7,6 +7,15 @@ import pyodbc
 # NOTE: For any other database engine, you need to change the DRIVER= variable in order to match
 #       a configuration tag in the /etc/odbcinst.ini
 #
+#       Oracle connection string requires the DBQ= variable
+#
+#	SQLServer connection string requires the TDS_Version= variable
+#	(use TDS_Version=8.0 for MSSQL2012
+#
+
+#conn = pyodbc.connect("DRIVER=Oracle11g;DBQ=//127.0.0.1:1521/dbname;UID=system;PWD=secret")
+#conn = pyodbc.connect("DRIVER=MSSQL;SERVER=127.0.0.1;PORT=3306;DATABASE=dbname;UID=administrator;PWD=secret;TDS_Version=8.0")
+
 conn = pyodbc.connect("DRIVER=MySQL;SERVER=127.0.0.1;PORT=3306;DATABASE=mysql;UID=root;PWD=secret")
 
 
